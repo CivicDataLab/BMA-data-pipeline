c9fa542a687ee799becaf27d0fb95f88bca76ec6
Prajna1999 <gituprajna20@gmail.com>
2024-12-13 14:29:54 +0530

    CHORE: remove redundant

--- datatransform/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Task, Pipeline
-
-admin.site.register(Task)
-admin.site.register(Pipeline)
--- datatransform/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DatatransformConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'datatransform'

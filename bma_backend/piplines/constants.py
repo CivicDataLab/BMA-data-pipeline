5ed1acdfb24359fa1228eba5fac282c2243afdf2
Prajna1999 <gituprajna20@gmail.com>
2024-12-17 13:29:07 +0530

    ADD: typer for CLI

--- bma_backend/piplines/constants.py
+++ bma_backend/piplines/constants.py
@@ -1,5 +1,7 @@
 GEOJSON_URLS = {
     "risk_points": "http://flood.bangkok.go.th:8443/geoserver/open_lift/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=open_lift%3Arisk_point&maxFeatures=50&outputFormat=application%2Fjson",
     "main_canal": "http://flood.bangkok.go.th:8443/geoserver/Bangkok_TH/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Bangkok_TH%3ACANAL_Main&maxFeatures=50&outputFormat=application%2Fjson",
-    "districts_boundary": "http://flood.bangkok.go.th:8443/geoserver/Bangkok_TH/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Bangkok_TH%3AADMIN_DISTRICT_BND&maxFeatures=50&outputFormat=application%2Fjson"
+    "districts_boundary": "http://flood.bangkok.go.th:8443/geoserver/Bangkok_TH/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Bangkok_TH%3AADMIN_DISTRICT_BND&maxFeatures=50&outputFormat=application%2Fjson",
+    "flood_complaints": "https://publicapi.traffy.in.th/teamchadchart-stat-api/geojson/v1?output_format=json&name=Prajna&org=Civicdatalab&purpose=Visualisation&email=prajna@civicdatalab.in&limit=1000"
+
 }
